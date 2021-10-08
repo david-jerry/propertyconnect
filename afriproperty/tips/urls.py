67d71a6d1c054c10f26e835b6c9f2275f7a41082
jeremiah david <jeremiahedavids@gmail.com>
2021-10-08 09:10:43 -0400

    removed sensitive files from public repo

--- afriproperty/tips/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from afriproperty.tips.views import TipDetailView, TipListView
-
-app_name = "tips"
-urlpatterns = [
-    path("", view=TipListView.as_view(), name="list"),
-    path("<slug>/", view=TipDetailView.as_view(), name="detail"),
-]
