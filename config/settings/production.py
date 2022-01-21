813c36367d7d75e1c48c17ddcc7e7bb7276cb2c0
Jeremiah David <jeremiahedavid@gmail.com>
2022-01-21 19:49:38 +0100

    fixed allowed hosts

--- config/settings/production.py
+++ config/settings/production.py
@@ -14,7 +14,7 @@ from .base import env
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["afriproperty.ng"])
+ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["afriproperty.ng", "www.afriproperty.ng"])
 PREPEND_WWW = False
 
 # DATABASES
